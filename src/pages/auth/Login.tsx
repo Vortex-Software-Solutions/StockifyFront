60b962328f93f898353c5b992e95c267d82d6662
rodrigoBojorquez <rbojorquez1620@gmail.com>
2024-09-18 22:51:21 -0500

    fix(config): cambios en la configuracion del linter

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -70,14 +70,14 @@ const Login: React.FC = () => {
     };
 
     return (
-        <main className="flex justify-center items-center bg-gray-100 min-h-screen">
+        <main className="flex justify-center items-center bg-gray-50 min-h-svh px-0 md:px-10">
             {isLoading && (
                 <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-gray-500 bg-opacity-50 z-50">
                     <LoaderBig message={'Espere...'} />
                 </div>
             )}
-            <div className="flex flex-col md:flex-row bg-white rounded-lg overflow-auto shadow-lg w-full max-w-4xl">
-                <div className="w-full md:w-1/2 p-8 flex flex-col justify-center">
+            <div className="flex flex-col md:flex-row flex-1 bg-white rounded-none md:rounded-lg border-none md:border-2 h-svh md:h-auto  items-center border-gray-100 overflow-auto shadow-xl w-full max-w-4xl">
+                <div className="w-full md:w-1/2 p-8 flex flex-col justify-center flex-1 grow">
                     <h1 className="text-center text-2xl font-bold mb-8">Bienvenido de nuevo</h1>
                     <form onSubmit={handleSubmit(submitForm)} className="space-y-6">
                         <div>
@@ -125,15 +125,21 @@ const Login: React.FC = () => {
                     </div>
                     <div className="mt-6 text-center">
                         <span className="text-gray-600">¿Aún no tienes cuenta? </span>
-                        <Link to="/register" className="text-[#6181F7] hover:underline">Crear Cuenta</Link>
+                        <Link to="/register" className="text-primary-purple hover:underline">Crear Cuenta</Link>
                     </div>
                 </div>
-                <div className="hidden md:block md:w-1/2 bg-[#6181F7] p-8 lg:p-20">
+
+
+                <div className="hidden md:flex md:w-1/2 bg-[#6181F7] flex-col flex-1 grow h-full">
                     <div className="flex flex-col items-center justify-center h-full">
-                        <h2 className="text-white text-3xl lg:text-4xl font-bold mb-8 ">Stockify.com</h2>
+                        <h2 className="text-white text-3xl lg:text-4xl font-moul mb-8 ">Stockify.com</h2>
                         <img src={Presentation} alt="Analytics Image" className="max-w-full h-auto" />
                     </div>
                 </div>
+
+                <footer className="bg-primary-purple w-full p-1 md:hidden">
+                    <p className="font-moul text-base text-white text-center">Stockify.com</p>
+                </footer>
             </div>
         </main>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-purple": "#6181F7",
+        "grey-dark": "#232931 ",
+        "grey-base": "#232931",
+        "grey-light": "#B1B1B1",
+      },
+      fontFamily: {
+        "inter": ["Inter", "sans-serif"],
+        "moul": ["Moul", "serif"],
+      }
+    },
   },
   plugins: [],
 }
